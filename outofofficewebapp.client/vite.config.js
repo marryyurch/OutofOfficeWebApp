3440de05cd296d37bdc23c26e0456bbf9bd214ef
marryyurch <114480776+marryyurch@users.noreply.github.com>
2024-06-15 22:06:04 +0300

    refreshed client files

--- outofofficewebapp.client/vite.config.js
+++ outofofficewebapp.client/vite.config.js
@@ -46,7 +46,23 @@ export default defineConfig({
             '^/weatherforecast': {
                 target,
                 secure: false
-            }
+            },
+            '^/pingauth': {
+                target,
+                secure: false
+            },
+            '^/register': {
+                target,
+                secure: false
+            },
+            '^/logout': {
+                target,
+                secure: false
+            },
+            '^/login': {
+                target,
+                secure: false
+            },
         },
         port: 5173,
         https: {
