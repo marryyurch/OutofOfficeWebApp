53132f39a47072569c2befd9dcd2d94122d501a4
marryyurch <114480776+marryyurch@users.noreply.github.com>
2024-06-15 23:52:36 +0300

    Added ProjectMember

---
+++ OutofOfficeWebApp.Server/Models/ProjectMember.cs
@@ -0,0 +1,17 @@
+﻿using OutofOfficeAPI.Models;
+
+namespace OutofOfficeWebApp.Server.Models
+{
+    public class ProjectMember
+    {
+        public int Id { get; set; }
+
+        public int EmployeeId { get; set; }
+
+        public int ProjectId { get; set; }
+
+        public Employee Employee { get; set; }
+
+        public Project Project { get; set; }
+    }
+}
