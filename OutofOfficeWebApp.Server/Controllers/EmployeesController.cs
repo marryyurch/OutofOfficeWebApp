2d56bd41da7a9e3ddc43d347d829434cfa5a8c58
marryyurch <114480776+marryyurch@users.noreply.github.com>
2024-06-15 22:13:43 +0300

    added update/filter/get methods

--- OutofOfficeWebApp.Server/Controllers/EmployeesController.cs
+++ OutofOfficeWebApp.Server/Controllers/EmployeesController.cs
@@ -1,8 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using OutofOfficeAPI.Data;
 using OutofOfficeAPI.Models;
-using System.Runtime.CompilerServices;
+using System.Linq.Expressions;
 
 namespace OutofOfficeAPI.Controllers
 {
@@ -16,8 +16,8 @@ namespace OutofOfficeAPI.Controllers
             _outofOfficeDbContext = outofOfficeDBContext;
         }
 
-        [HttpPost]
-        public async Task<IActionResult> AddEmployee(Employee employee)
+        [HttpPost("add-emplpoyee")]
+        public async Task<IActionResult> AddEmployee([FromBody] Employee employee)
         {
             _outofOfficeDbContext.Employees.Add(employee);
             await _outofOfficeDbContext.SaveChangesAsync();
@@ -25,7 +25,7 @@ namespace OutofOfficeAPI.Controllers
             return Ok(employee);
         }
 
-        [HttpDelete]
+        [HttpDelete("delete-employee")]
         public async Task<IActionResult> DeleteEmployee(int id)
         {
             var employee = await _outofOfficeDbContext.Employees.FindAsync(id);
@@ -35,6 +35,65 @@ namespace OutofOfficeAPI.Controllers
             await _outofOfficeDbContext.SaveChangesAsync();
 
             return NoContent();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetEmployee([FromQuery]string? search, string? sortItem, string? sortOrder)
+        {
+            var employeesQuery = _outofOfficeDbContext.Employees
+                .Where(e => string.IsNullOrWhiteSpace(search) ||
+                e.FullName.ToLower().Contains(search.ToLower()));
+
+            Expression<Func<Employee, object>> selectorKey = sortItem?.ToLower() switch
+            {
+                "name" => employee => employee.FullName,
+                "subdivision" => employee => employee.SubdivisionType,
+                "position" => employee => employee.PositionType,
+                "status" => employee => employee.StatusType,
+                "ppartner" => employee => employee.PeoplePartner,
+                "ooobalance" => employee => employee.OutofOfficeBalance,
+                _ => employee => employee.Id
+            };
+
+            employeesQuery = sortOrder == "desc"
+                ? employeesQuery.OrderByDescending(selectorKey)
+                : employeesQuery.OrderBy(selectorKey);
+
+            return Ok(await employeesQuery.ToListAsync());
+        }
+
+        [HttpGet("filter-employee")]
+        public async Task<IActionResult> FilterEmployee(string filterItem, string itemContent)
+        {
+            var employeesQuery = _outofOfficeDbContext.Employees;
+
+            var employees = filterItem.ToLower() switch
+            {
+                "name" => employeesQuery.Where(e => e.FullName == itemContent),
+                "subdivision" => employeesQuery.Where(e => (int)e.SubdivisionType == int.Parse(itemContent)),
+                "position" => employeesQuery.Where(e => (int)e.PositionType == int.Parse(itemContent)),
+                "status" => employeesQuery.Where(e => (int)e.StatusType == int.Parse(itemContent)),
+                "ppartner" => employeesQuery.Where(e => e.PeoplePartner == int.Parse(itemContent)),
+                _ => employeesQuery
+            };
+
+            return Ok(employees);
+        }
+
+        [HttpPost("update-employee")]
+        public async Task<IActionResult> UpdateEmployee(Employee employee)
+        {
+            //update and deactivate
+
+            var employeesQuery = _outofOfficeDbContext.Employees;
+
+            employeesQuery.Remove(employeesQuery.Where(e => e.Id == employee.Id).First());
+
+            employeesQuery.Add(employee);
+
+            await _outofOfficeDbContext.SaveChangesAsync();
+
+            return Ok("Done");
         }
     }
 }
