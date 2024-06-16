05bc28d18380d95a44c6e3f3819326124461b8e9
marryyurch <114480776+marryyurch@users.noreply.github.com>
2024-06-16 18:39:53 +0300

    drop enums to separate folder

---
+++ OutofOfficeWebApp.Server/Enums/Enums.cs
@@ -0,0 +1,51 @@
+﻿namespace OutofOfficeWebApp.Server.Enums
+{
+    public enum SubdivisionType
+    {
+        Marketing = 0,
+        Sales = 1,
+        HR = 2,
+        Development = 3,
+        Support = 4
+    }
+
+    public enum PositionType
+    {
+        Manager = 0,
+        Analyst = 1,
+        Tester = 2,
+        Designer = 3,
+        Developer = 4
+    }
+
+    public enum StatusType
+    {
+        Inactive = 0,
+        Active = 1
+    }
+
+    public enum AbsenceReasonType
+    {
+        Vacation = 0,
+        SickLeave = 1,
+        UnpaidLeave = 2,
+        Other = 3
+    }
+
+    public enum RequestStatusType
+    {
+        New = 0,
+        Approved = 1,
+        Rejected = 2,
+        Canceled = 3,
+        Submitted = 4
+    }
+
+    public enum ProjectType
+    {
+        Research = 1,
+        Development = 2,
+        Testing = 3,
+        Deactivated = 4
+    }
+}
