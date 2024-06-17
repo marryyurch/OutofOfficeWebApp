04cc0f280d4c5012ff2f8890a28229d64c71a210
marryyurch <114480776+marryyurch@users.noreply.github.com>
2024-06-17 11:31:33 +0300

    configure source files

--- outofofficewebapp.client/src/App.jsx
+++ outofofficewebapp.client/src/App.jsx
@@ -1,15 +1,25 @@
+import { } from "@chakra-ui/react";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './index.css';
 import './App.css';
-
-import { BrowserRouter, Routes } from 'react-router-dom';
+import Home from './Pages/Home.jsx';
+import Start from './Pages/Start.jsx';
+import Login from './Pages/Login.jsx';
+import Register from './Pages/Register.jsx';
+import RegisterHR from './Pages/RegisterHR.jsx';
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
-                
+                <Route path="/" element={<Start />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/registerHR" element={<RegisterHR />} />
+                <Route path="/home" element={<Home />} />
             </Routes>
         </BrowserRouter>
     );
-
 }
+
 export default App;
